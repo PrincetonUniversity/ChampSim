4ef3fd525ff54d665d1dffd25230c3292aca8f2c
Bhargav Reddy Godala <bgodala@princeton.edu>
2023-08-30 13:11:44 -0400

    Added compilabled code from CBP-2016 TAGE_SC_L

---
+++ branch/tage_sc_l/tage_sc_l.cc
@@ -0,0 +1,11 @@
+///////////////////////////////////////////////////////////////////////
+////  Copyright 2015 Samsung Austin Semiconductor, LLC.                //
+/////////////////////////////////////////////////////////////////////////
+
+#include "tage_sc_l.h"
+
+bool tage_sc_l::predict_branch(uint64_t pc){
+}
+
+void tage_sc_l::last_branch_result(uint64_t pc, uint64_t branch_target, uint8_t taken, uint8_t branch_type){
+}
